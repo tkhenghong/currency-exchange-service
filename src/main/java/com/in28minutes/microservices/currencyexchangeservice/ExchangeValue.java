c9454e4c4dd5e063f9d90faa7a2b639ff851e218
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-20 07:08:34 +0800

    Created simple controller method

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,38 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple; // conversion factor, currency A * converionMultiple = currency B
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        super(); // super() is written in Eclipse IDE, interesting!
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    // Make empty constructors to make JPA happy
+    public ExchangeValue() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+}
